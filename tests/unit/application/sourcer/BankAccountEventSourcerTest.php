19e3089217fa97f895a3dd43f17095f8f84988f1
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-12-28 08:56:05 +0100

    Improve TestDox output

--- tests/unit/application/sourcer/BankAccountEventSourcerTest.php
+++ tests/unit/application/sourcer/BankAccountEventSourcerTest.php
@@ -14,6 +14,7 @@ use example\framework\event\EventReader;
 use example\framework\library\Uuid;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\TestCase;
 
@@ -28,6 +29,7 @@ use PHPUnit\Framework\TestCase;
 #[UsesClass(MoneyWithdrawnEvent::class)]
 #[UsesClass(Uuid::class)]
 #[Small]
+#[TestDox('BankAccountEventSourcer')]
 final class BankAccountEventSourcerTest extends TestCase
 {
     public function testSourcesBankAccountFromEvents(): void
